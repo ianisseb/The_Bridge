61d5ab1f96a24623d42e9b7e1f528420c2450290
Ianis <apetreiianis12@gmail.com>
2024-05-22 14:40:29 +0300

    conversations

---
+++ app/seller/[username]/manage-gigs/_components/gig-card/index.tsx
@@ -0,0 +1,9 @@
+import { Doc } from "@/convex/_generated/dataModel";
+
+interface GigCardProps {
+  gig: Doc<"gigs">;
+}
+
+export const GigCard = ({ gig }: GigCardProps) => {
+  return <div>{gig.title}</div>;
+};
